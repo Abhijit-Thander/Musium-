2b8683c66ad64df24ff53f591524022ae4fe8af5
Abhijit <abhijitthander38@gmail.com>
2025-08-02 20:36:50 +0530

    explore page search bar

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,27 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
+import Colors from "@/constants/Colors";
+import { StyleSheet, Text, TextInput, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={require("@assets/images/musium.png")}
+          style={styles.img}
+        />
+        <Text style={styles.title}>Search</Text>
+      </View>
+
+      <TextInput
+        placeholder=" songs, artist, album, & more"
+        placeholderTextColor="#d7d7d7"
+        autoCapitalize="none"
+        style={styles.input}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 16,
+    backgroundColor: Colors.background,
+  },
+  img: {
+    width: 40,
+    height: 30,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    color: Colors.accent,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  input: {
+    color: "#000",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 50,
+    marginVertical: 16,
+    backgroundColor: "#fff",
   },
 });
